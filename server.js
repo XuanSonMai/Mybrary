afdb05783d3ed1ac737ea28f502eb2ffe6b1ed1d
Son Mai <mxsbettadragon97@gmail.com>
2022-03-21 09:33:05 +0700

    aa

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set(express.static('public'))
 
 //mongoose
 const mongoose=require('mongoose')
-mongoose.connect('//127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false',{
+mongoose.connect('mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false',{
     useNewUrlParser: true
 })
 const db = mongoose.connection
