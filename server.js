3671f21d5f5e67ba6764ccea67a5af067984efb5
Son Mai <mxsbettadragon97@gmail.com>
2022-03-21 09:31:12 +0700

    ss

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set(express.static('public'))
 
 //mongoose
 const mongoose=require('mongoose')
-mongoose.connect(process.env.DB_URI,{
+mongoose.connect('//127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false',{
     useNewUrlParser: true
 })
 const db = mongoose.connection
